a9b7d894f1d3f504707d3586271fc6769833f59a
Hrusteckiy <kostia4554t@yandex.ru>
2024-05-17 09:27:51 +0300

    Новый шрифт для визуального выделения текста в консоли

---
+++ shaders/r2/hud_font2.s
@@ -0,0 +1,8 @@
+function normal		(shader, t_base, t_second, t_detail)
+	shader:begin	("null","font2")
+			: fog		(false)
+			: zb 		(false,false)
+			: blend		(true,blend.srcalpha,blend.invsrcalpha)
+			: fmin_aniso()
+ 	shader:sampler	("s_base")       :texture  (t_base)
+end

ed82b12187a3ca22bdc11215dbfa307bb798f846
Vector Infamous <vectorqt@gmail.com>
2025-07-17 22:56:23 +0300

    Ненужные реализации в R1

--- gamedata/shaders/r1/effects_water.s
+++
@@ -1,8 +0,0 @@
-function normal(shader, t_base, t_second, t_detail)
-	shader:begin("vert","vert")
-			: sorting	(2, true)
-			: blend		(true,blend.srcalpha,blend.invsrcalpha)
-			: zb 		(true,false)
-	shader:sampler("s_base"):texture(t_base)
-	shader:sampler("s_position"):texture("$user$position")
-end

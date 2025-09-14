a73ff54066dee22042d2b8bfee5132f5bf626e88
theysani <theysani@vk.com>
2025-09-14 18:00:51 +0500

    Revert "В задании на гравимолот в диалоге когда информатор выдаёт награду теперь выводится сообщение о получении итема"
    This reverts commit 944f140d380ebc5848a5992c8b8d5d6e220eac99.

--- gamedata/shaders/r1 — копия/hud_crosshair.s
+++
@@ -1,6 +0,0 @@
-function normal		(shader, t_base, t_second, t_detail)
-	shader:begin		("null","hud_crosshair")
-			: fog		(false)
-			: zb 		(false,false)
-			: blend		(true,blend.srcalpha,blend.invsrcalpha)
-end

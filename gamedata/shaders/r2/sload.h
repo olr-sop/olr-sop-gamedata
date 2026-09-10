1da99884b41ef04111d832977f4ec626a5798ddb
theysani <theysani@vk.com>
2026-09-10 23:06:16 +0500

    Заготовка под пайплайн со спекуляром. Закинут под дефайны, отключён по дефолту на данный момент

--- gamedata/shaders/r2/sload.h
+++ gamedata/shaders/r2/sload.h
@@ -3,74 +3,103 @@
 
 #include "common.h"
 
+// Для террейна нужно будет отдельно переделывать шейдер под новый пайплайн
+
+//#define USE_NEW_PIPELINE	 // Подключение нового пайплайна со спекуляром
+//#define USE_PLUS_Y_NRM	 // Использовать +Y (OGL) карту нормалей
+//#define USE_SEPARATE_SPEC	 // Отдельный спекуляр в г-буффер (требуется полное ковыряние всего для его использования в остальных местах)
+
 //////////////////////////////////////////////////////////////////////////////////////////
-// Bumped surface loader                //
+// Bumped surface loader				//
 //////////////////////////////////////////////////////////////////////////////////////////
-struct        surface_bumped
+struct surface_bumped
 {
-        float4         	base;
-        float3         	normal;
-        float         	gloss;
-        float         	height;
+	float4 base;
+	float3 normal;
+	float  gloss;
+	float  height;
+#ifdef USE_SEPARATE_SPEC
+	float  spec;
+#endif
 };
 
-#ifdef         DBG_TMAPPING
-float4   tbase                 (float2 tc)        {
-        float2                 tile                 = max(ddx(tc),ddy(tc));
-        return                 (1-max(tile.x,tile.y));        //*tex2D        (s_base,         tc);
+#ifdef DBG_TMAPPING
+float4 tbase(float2 tc)
+{
+	float2 tile = max(ddx(tc), ddy(tc));
+	return (1 - max(tile.x, tile.y)); //*tex2D(s_base, tc);
 }
 #else
-float4   tbase                 (float2 tc)        {
-        return                 tex2D                (s_base,         tc);
-}
-#endif
-
-#ifdef         USE_PARALLAX
-surface_bumped                sload_i         ( p_bumped I)        // + texld, mad, nrm(3), mad  = 1+1+3+1 = 6, 15+6=21, OK
+float4 tbase(float2 tc)
 {
-        surface_bumped      S;
-        float        height	=       tex2D      (s_bumpX, I.tcdh).w;                                //
-                    height  =       height*parallax.x + parallax.y;                        //
-        float2		new_tc  =       I.tcdh + height*normalize        (I.eye);                //
-        float4       Nu      =       tex2D		(s_bump,         new_tc);                // IN:  normal.gloss
-        float4       NuE     =       tex2D      	(s_bumpX,       new_tc);                // IN:         normal_error.height
-        S.base              =       tbase                (new_tc);                                // IN:  rgb.a
-        S.normal            = 		Nu.wzy + (NuE.xyz - 1.0h);							 // (Nu.wzyx - .5h) + (E-.5)
-        S.gloss             =       Nu.x*Nu.x	;                                        //        S.gloss             =        Nu.x*Nu.x;
-        S.height            =       NuE.w       ;
-
-#ifdef        USE_TDETAIL
-        float4       detail  =		tex2D(s_detail,I.tcdbump)        	;
-        S.base.rgb          =		S.base.rgb     * detail.rgb*2		;
-        S.gloss             =  		S.gloss * detail.w * 2				;
+	return tex2D(s_base, tc);
+}
 #endif
 
-        return                S;
-}
+surface_bumped sload_i(p_bumped I)
+{
+	surface_bumped S;
+	float2 tc = I.tcdh;
+
+	// Параллакс
+#ifdef USE_PARALLAX
+	// Выбор канала высоты в зависимости от пайплайна
+	#if defined(USE_NEW_PIPELINE)
+		#define HEIGHT_CHANNEL g
+	#else
+		#define HEIGHT_CHANNEL w
+	#endif
+	float height = tex2D(s_bumpX, I.tcdh).HEIGHT_CHANNEL;
+	height = height * parallax.x + parallax.y;
+	tc = I.tcdh + height * normalize(I.eye);
+	#undef HEIGHT_CHANNEL
+#endif
+
+#if defined(USE_NEW_PIPELINE)
+	float4 Nu  = tex2D(s_bump,	tc);	// r=gloss, g=-Y, a=+X
+	float4 NuE = tex2D(s_bumpX, tc);	// g=height, a=spec
+	float2 nXY = Nu.wy * 2.f - 1.f;		// A=X, G=Y, full range
+	S.base	   = tbase(tc);
+	S.normal   = float3(nXY, sqrt(saturate(1.f - dot(nXY, nXY))));
+	#if defined(USE_PLUS_Y_NRM)
+		S.normal.y = -S.normal.y;		// Переворачиваем огл нормали в дх
+	#endif
+	#if defined(USE_SEPARATE_SPEC)
+		S.gloss = Nu.x * Nu.x;			// gloss^2
+		S.spec	= NuE.a;				// spec
+	#else
+		S.gloss = Nu.x * Nu.x * NuE.a;	// gloss^2 * spec
+	#endif
+	S.height = NuE.g;					// высота из канала G
 #else
-surface_bumped                sload_i         ( p_bumped I)
-{
-        surface_bumped        S;
-          float4 Nu 			=		tex2D                (s_bump, I.tcdh);                        // IN:  normal.gloss
-        float4 NuE           =		tex2D                (s_bumpX,I.tcdh);                        // IN:         normal_error.height
-        S.base              =		tbase                (I.tcdh)		;                         // IN:  rgb.a
-        S.normal            =		Nu.wzy + (NuE.xyz - 1.0h)			;
-        S.gloss             =		Nu.x*Nu.x							;                         //        S.gloss             =        Nu.x*Nu.x;
-        S.height            = 		NuE.w;
-
-#ifdef        USE_TDETAIL
-        float4 detail		=        tex2D(s_detail,I.tcdbump)    ;
-        S.base.rgb          =      	S.base.rgb*detail.rgb        	*2      ;
-        S.gloss             =  		S.gloss * detail.w * 2			;
+	// ориг пайплайн
+	float4 Nu  = tex2D(s_bump,	tc);
+	float4 NuE = tex2D(s_bumpX, tc);
+	S.base	   = tbase(tc);
+	S.normal   = Nu.wzy + (NuE.xyz - 1.0f);
+	S.gloss	   = Nu.x * Nu.x;
+	S.height   = NuE.w;
+	#if defined(USE_SEPARATE_SPEC)
+		S.spec = 1.0f;					// Если отдельный spec включён без нового пайплайна
+	#endif
 #endif
-        return              S;
+
+#ifdef USE_TDETAIL
+	float4 detail = tex2D(s_detail, I.tcdbump);
+	S.base.rgb *= detail.rgb * 2;
+	S.gloss	   *= detail.w * 2;
+	#if defined(USE_SEPARATE_SPEC)
+		S.spec *= detail.w * 2;
+	#endif
+#endif
+
+	return S;
 }
-#endif
 
-surface_bumped              sload                 ( p_bumped I)
+surface_bumped sload(p_bumped I)
 {
-        surface_bumped      S   = sload_i	(I);
-        return              S;
+	surface_bumped S = sload_i(I);
+	return S;
 }
 
 #endif
